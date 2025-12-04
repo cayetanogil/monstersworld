9a4de1ceb9ee9c579073496a5dba555079d80483
Cayetano Gil <cayetanogilsanchez@gmail.com>
2025-12-04 00:11:11 -0800

    fixing rendering issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
+    autoprefixer: {}, // optional but you already have it installed
   },
 };
 
