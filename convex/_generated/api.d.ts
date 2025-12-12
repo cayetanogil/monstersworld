5ad998f6ea4f13cce699aafc68443515e2d56f80
Cayetano Gil <cayetanogilsanchez@gmail.com>
2025-12-11 22:46:07 -0800

    deps

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,31 +8,44 @@
  * @module
  */
 
+import type * as guesses from "../guesses.js";
+import type * as locations from "../locations.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as guesses from "../guesses.js";
-import type * as locations from "../locations.js";
+
+declare const fullApi: ApiFromModules<{
+  guesses: typeof guesses;
+  locations: typeof locations;
+}>;
 
 /**
- * A utility for referencing Convex functions in your app's API.
+ * A utility for referencing Convex functions in your app's public API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  guesses: typeof guesses;
-  locations: typeof locations;
-}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
